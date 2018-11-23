b39cc653ed4e7c9e58a78aca645717bab3abac20
kevintijssen <kevin.tijssen@gmail.com>
2018-11-23 13:44:22 +0100

    Update Ansible_WinRM.ps1

--- Ansible_WinRM.ps1
+++ Ansible_WinRM.ps1
@@ -2,4 +2,3 @@ Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
 Enable-PSRemoting -Force -ErrorAction Stop
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
--NetworkCategory Private 
